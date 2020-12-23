4a3a7608dc9ed03fc3334552b87390ae9dcbca92
Frederik Glücks <frederik@gluecks-gmbh.de>
2020-12-24 00:11:09 +0100

    add class/function to create a http call without waiting for a response by using a thread

--- luckywood/__init__.py
+++ luckywood/__init__.py
@@ -4,8 +4,10 @@ Luckywood python package
 
 from .database import Database
 from .data_caching import DataCaching
+from .async_http_call import AsyncHttpCall
 
 __all__ = [
     'Database',
-    'DataCaching'
+    'DataCaching',
+    'AsyncHttpCall'
 ]
---
+++ luckywood/async_http_call.py
@@ -0,0 +1,66 @@
+"""
+Class to create an http call without waiting of a response.
+"""
+
+__author__ = "Frederik Glücks"
+__email__ = "frederik@gluecks-gmbh.de"
+__copyright__ = "Frederik Glücks - Glücks GmbH"
+
+# Generic/Built-in Imports
+import threading
+from typing import Dict
+
+# External libs/modules
+import requests
+
+
+class AsyncHttpCall:
+    """
+    Class to create an http call without waiting of a response.
+    """
+
+    @staticmethod
+    def start(url: str, method: str = "GET", payload: str = "", bearer_token: str = ""):
+        """
+        Starts a thread that calls a http call without waiting for a response
+
+        :param url: str
+        :param method: str
+        :param payload: json
+        :param bearer_token: str
+        :return: None
+        """
+        threading.Thread(target=AsyncHttpCall.thread_call, args=(url, method.upper(), payload, bearer_token)).start()
+
+    @staticmethod
+    def thread_call(url: str, method: str, payload: str, bearer_token: str):
+        """
+        Runs the http call by http method
+
+        :param url: str
+        :param method: str
+        :param payload: str
+        :param bearer_token: str
+        :return: None
+        """
+
+        if bearer_token != "":
+            headers: Dict[str, str] = {"Authorization": "Bearer " + bearer_token}
+        else:
+            headers: Dict[str, str] = {}
+
+        if method == 'GET':
+            requests.get(url, data=payload, headers=headers)
+
+            if payload != {}:
+                raise Warning("HTTP method get should not have a payload!")
+        elif method == 'PUT':
+            requests.put(url, data=payload, headers=headers)
+        elif method == 'POST':
+            requests.post(url, data=payload, headers=headers)
+        elif method == 'DELETE':
+            requests.delete(url, headers=headers)
+        elif method == 'OPTIONS':
+            requests.options(url, headers=headers)
+        else:
+            raise RuntimeError("unknown http method {}".format(method))
