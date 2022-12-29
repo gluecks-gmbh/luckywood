0971ffe16ef6ecb7d82956fd89588b71e2b12016
Frederik Glücks <frederik@gluecks-gmbh.de>
2022-12-30 00:02:51 +0100

    Version auf 0.6.0 erhöht

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='luckywood',
-    version='0.5.4',
+    version='0.6.0',
     description='Package of helper classes.',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
